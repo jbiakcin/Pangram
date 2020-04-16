e316721bf70b75c93e68741dd88bc4c303556642
Jennie R <jbiakcin@icloud.com>
2020-04-15 21:46:51 -0700

    Add more game letters

--- src/gameLetters.js
+++ src/gameLetters.js
@@ -63,6 +63,90 @@ letters = [
     reqLetters: ["I"],
     optLetters: ["G", "P", "R", "A", "L", "Y"]
   },
+  {
+    reqLetters: ["A"],
+    optLetters: ["C", "D", "I", "L", "M", "T"]
+  },
+  {
+    reqLetters: ["L"],
+    optLetters: ["B", "D", "G", "I", "N", "Y"]
+  },
+  {
+    reqLetters: ["H"],
+    optLetters: ["A", "C", "E", "I", "R", "T"]
+  },
+  {
+    reqLetters: ["Y"],
+    optLetters: ["A", "D", "G", "I", "L", "U"]
+  },
+  {
+    reqLetters: ["N"],
+    optLetters: ["B", "G", "H", "I", "L", "O"]
+  },
+  {
+    reqLetters: ["R"],
+    optLetters: ["C", "E", "K", "O", "P", "W"]
+  },
+  {
+    reqLetters: ["E"],
+    optLetters: ["A", "B", "D", "L", "O", "W"]
+  },
+  {
+    reqLetters: ["T"],
+    optLetters: ["A", "C", "E", "I", "N", "O", "U"]
+  },
+  {
+    reqLetters: ["E"],
+    optLetters: ["A", "C", "H", "M", "N", "T"]
+  },
+  {
+    reqLetters: ["E"],
+    optLetters: ["A", "H", "L", "O", "P", "R"]
+  },
+  {
+    reqLetters: ["I"],
+    optLetters: ["A", "B", "E", "L", "R", "Z"]
+  },
+  {
+    reqLetters: ["E"],
+    optLetters: ["B", "C", "D", "K", "L", "U"]
+  },
+  {
+    reqLetters: ["P"],
+    optLetters: ["C", "D", "E", "L", "N", "U"]
+  },
+  {
+    reqLetters: ["O"],
+    optLetters: ["A", "G", "H", "I", "N", "X"]
+  },
+  {
+    reqLetters: ["E"],
+    optLetters: ["F", "G", "I", "N", "R", "Z"]
+  },
+  {
+    reqLetters: ["R"],
+    optLetters: ["A", "C", "D", "E", "H", "T"]
+  },
+  {
+    reqLetters: ["T"],
+    optLetters: ["A", "C", "E", "D", "O", "V"]
+  },
+  {
+    reqLetters: ["R"],
+    optLetters: ["A", "E", "F", "M", "O", "T"]
+  },
+  {
+    reqLetters: ["D"],
+    optLetters: ["E", "I", "M", "N", "R", "T"]
+  },
+  {
+    reqLetters: ["M"],
+    optLetters: ["A", "C", "E", "I", "N", "T"]
+  },
+  {
+    reqLetters: [],
+    optLetters: []
+  },
 
 ]
 
